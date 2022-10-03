1bf246955f71024e28d7dbdfc52a808d038fda4c
Dale <172586304@qq.com>
2022-10-03 09:41:44 +0800

    first commit

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @author Dale
+ * @create 2022-10-03 9:38
+ * @description
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
--- test1.cpp
+++
@@ -1,8 +0,0 @@
-#include<cstdio>
-void f(int x[][2]){
-	printf("%d",x[1][1]);
-}
-int main(){
-	int a[2][2]={0,1,2,3};
-	f(a);
-} 
