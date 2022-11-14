555dc30ef8aac2756b6fb0e70b00f144839e89d1
Dale <172586304@qq.com>
2022-11-14 09:32:36 +0800

    updating...

---
+++ src/DMSXL/Array/Test0704.java
@@ -0,0 +1,31 @@
+package DMSXL.Array;
+
+/**
+ * @author Dale
+ * @create 2022-11-14 9:24
+ * @description
+ */
+public class Test0704 {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9;
+        int res = search(nums, target);
+        System.out.println(res);
+    }
+
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int middle = left + ((right - left) / 2);
+            if (nums[middle] > target) {
+                right = middle - 1;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+}
