fab94756faa8014fbaea3bc191f16583bc20db83
Dale <172586304@qq.com>
2022-11-21 22:06:34 +0800

    add test0063

--- src/programmer/Dynamic/Test0063.java
+++ src/programmer/Dynamic/Test0063.java
@@ -9,6 +9,29 @@ import java.util.Arrays;
  */
 public class Test0063 {
     public static void main(String[] args) {
+        int[][] obstacleGrid = {{0,0,0},{0,1,0},{0,0,0}};
+        int res = uniquePathsWithObstacles(obstacleGrid);
+        System.out.println(res);
+    }
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        //初始化
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n && obstacleGrid[0][j] == 0; j++) {
+            dp[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
 
     }
 }
