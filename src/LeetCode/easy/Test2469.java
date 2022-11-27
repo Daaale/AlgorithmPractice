1e6fb601670b2fa4b50de7b7a45999670dcbc517
Dale <172586304@qq.com>
2022-11-27 13:37:15 +0800

    updating...easy

---
+++ src/LeetCode/easy/Test2469.java
@@ -0,0 +1,22 @@
+package LeetCode.easy;
+
+import java.util.Arrays;
+
+/**
+ * @author Dale
+ * @create 2022-11-27 13:10
+ * @description
+ */
+public class Test2469 {
+    public static void main(String[] args) {
+        double celsius = 36.50;
+        double[] res = convertTemperature(celsius);
+        System.out.println(Arrays.toString(res));
+    }
+    public static double[] convertTemperature(double celsius) {
+        double[] res = new double[2];
+        res[0] = celsius + 273.15;
+        res[1] = celsius * 1.8 + 32.00;
+        return res;
+    }
+}
