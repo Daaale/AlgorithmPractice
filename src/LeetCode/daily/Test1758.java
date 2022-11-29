9cb9e64ed98cb7caff5c1d3bf3c5bf307a712f35
Dale <172586304@qq.com>
2022-11-29 15:17:39 +0800

    updating daily

---
+++ src/LeetCode/daily/Test1758.java
@@ -0,0 +1,17 @@
+package LeetCode.daily;
+
+/**
+ * @Author Dale
+ * @Date 2022/11/29 15:15
+ * @Description
+ */
+public class Test1758 {
+    public static void main(String[] args) {
+        String s = "0100";
+        int res = minOperations(s);
+        System.out.println(res);
+    }
+    public static int minOperations(String s) {
+        return 0;
+    }
+}
