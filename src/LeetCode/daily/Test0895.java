57ab7041117e440162e0138f2609af2c4acad77e
Dale <172586304@qq.com>
2022-12-01 00:39:22 +0800

    updating...

---
+++ src/LeetCode/daily/Test0895.java
@@ -0,0 +1,28 @@
+package LeetCode.daily;
+
+/**
+ * @author Dale
+ * @create 2022-11-30 13:10
+ * @description
+ */
+public class Test0895 {
+    public static void main(String[] args) {
+        FreqStack obj = new FreqStack();
+        obj.push(1);
+        int param_2 = obj.pop();
+    }
+     static class FreqStack {
+
+        public FreqStack() {
+
+        }
+
+        public void push(int val) {
+
+        }
+
+        public int pop() {
+
+        }
+    }
+}
