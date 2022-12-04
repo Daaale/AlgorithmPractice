234bf9afe9a00a3298592ed9c8cec7e49b0580d8
Dale <172586304@qq.com>
2022-12-04 20:01:04 +0800

    add easy

---
+++ src/LeetCode/lceasy/Test2351.java
@@ -0,0 +1,27 @@
+package LeetCode.lceasy;
+
+/**
+ * @author Dale
+ * @create 2022-12-04 17:19
+ * @description
+ */
+public class Test2351 {
+    public static void main(String[] args) {
+        String s = "abccbaacz";
+        char res = repeatedCharacter(s);
+        System.out.println(res);
+    }
+    public static char repeatedCharacter(String s) {
+        char res = 'a';
+        int[] cnt = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            cnt[c - 'a'] ++;
+            if (cnt[c - 'a'] > 1) {
+                res = c;
+                break;
+            }
+        }
+        return res;
+    }
+}
