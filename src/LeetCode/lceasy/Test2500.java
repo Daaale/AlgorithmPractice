940a117813529a378d70de42c787a73a532c488c
Dale <172586304@qq.com>
2023-03-12 15:20:36 +0800

    add ...

---
+++ src/LeetCode/lceasy/Test2500.java
@@ -0,0 +1,33 @@
+package LeetCode.lceasy;
+
+import java.lang.reflect.GenericArrayType;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+/**
+ * @author Dale
+ * @create 2023-03-12 15:13
+ * @description
+ */
+public class Test2500 {
+    public static void main(String[] args) {
+        int[][] grid = {{1,2,4},{3,3,1}};
+        int res = deleteGreatestValue(grid);
+        System.out.println(res);
+    }
+    public static int deleteGreatestValue(int[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            Arrays.sort(grid[i]);
+        }
+
+        for (int i = 0; i < grid[0].length; i++) {
+            int max = -1;
+            for (int j = 0; j < grid.length; j++) {
+                if (grid[j][i] > max) max = grid[j][i];
+            }
+            res += max;
+        }
+        return res;
+    }
+}
