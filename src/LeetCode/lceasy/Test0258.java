833326468781d18234192e438f4928fc9054ebf2
Dale <172586304@qq.com>
2023-03-14 22:40:22 +0800

    add daily...

---
+++ src/LeetCode/lceasy/Test0258.java
@@ -0,0 +1,25 @@
+package LeetCode.lceasy;
+
+/**
+ * @Author Dale
+ * @Date 2023/3/14 22:36
+ * @Description
+ */
+public class Test0258 {
+    public static void main(String[] args) {
+        int num = 38;
+        int res = addDigits(num);
+        System.out.println(res);
+    }
+    public static int addDigits(int num) {
+        while (num >= 10) {
+            String value = String.valueOf(num);
+            int temp = 0;
+            for (int i = 0; i < value.length(); i++) {
+                temp += (value.charAt(i) - '0');
+            }
+            num = temp;
+        }
+        return num;
+    }
+}
