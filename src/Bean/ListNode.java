df68abc8469bf137b6f2ea2096eb8a1cefb2135c
Dale <172586304@qq.com>
2023-03-15 19:15:42 +0800

    add practise

---
+++ src/Bean/ListNode.java
@@ -0,0 +1,16 @@
+package Bean;
+
+/**
+ * @author Dale
+ * @create 2023-03-15 18:35
+ * @description
+ */
+public class ListNode {
+    public int val;
+
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/LeetCode/lcmedium/Test0237.java
@@ -0,0 +1,27 @@
+package LeetCode.lcmedium;
+
+import Bean.ListNode;
+
+/**
+ * @author Dale
+ * @create 2023-03-15 18:35
+ * @description
+ */
+public class Test0237 {
+    public static void main(String[] args) {
+        ListNode root = new ListNode(4);
+        root.next = new ListNode(5);
+        root.next.next = new ListNode(1);
+        root.next.next.next = new ListNode(9);
+        deleteNode(root.next);
+        System.out.println(111);
+    }
+    public static void deleteNode(ListNode node) {
+        while (node.next.next != null) {
+            node.val = node.next.val;
+            node = node.next;
+        }
+        node.val = node.next.val;
+        node.next = null;
+    }
+}
