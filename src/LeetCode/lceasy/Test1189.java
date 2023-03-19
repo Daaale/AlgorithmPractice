d7505452ca587284b162e0ab36ffdcf315748d71
Dale <172586304@qq.com>
2023-03-19 09:42:16 +0800

    add lceasy...

---
+++ src/LeetCode/lceasy/Test1189.java
@@ -0,0 +1,41 @@
+package LeetCode.lceasy;
+
+/**
+ * @author Dale
+ * @create 2023-03-19 9:08
+ * @description
+ */
+public class Test1189 {
+    public static void main(String[] args) {
+        String text = "nlaebolko";
+        int res = maxNumberOfBalloons(text);
+        System.out.println(res);
+    }
+
+    public static int maxNumberOfBalloons(String text) {
+        String balloon = "balloon";
+        int[] cnts = new int[26];
+        for (int i = 0; i < balloon.length(); i++) {
+            cnts[balloon.charAt(i) - 'a'] ++;
+        }
+        int res = 0;
+        int[] textCnts = new int[26];
+        for (int i = 0; i < text.length(); i++) {
+            textCnts[text.charAt(i) - 'a'] ++;
+        }
+        boolean flag = true;
+        while (flag) {
+            for (int i = 0; i < 26; i++) {
+                textCnts[i] -= cnts[i];
+                if (textCnts[i] < 0) {
+                    flag = false;
+                    break;
+                }
+                if (i == 25) {
+                    res ++;
+                }
+            }
+        }
+        return res;
+    }
+}
