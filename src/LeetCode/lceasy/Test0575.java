4b2d48d5acc86750307d7d6d6dd08b459b3cad62
Dale <172586304@qq.com>
2023-03-19 09:58:18 +0800

    add ...

---
+++ src/LeetCode/lceasy/Test0575.java
@@ -0,0 +1,30 @@
+package LeetCode.lceasy;
+
+import java.util.HashSet;
+
+/**
+ * @author Dale
+ * @create 2023-03-19 9:44
+ * @description
+ */
+public class Test0575 {
+    public static void main(String[] args) {
+        int[] candyType = {1,1,2,2,3,3};
+        int res = distributeCandies(candyType);
+        System.out.println(res);
+    }
+    public static int distributeCandies(int[] candyType) {
+        HashSet<Integer> set = new HashSet<>();
+        int cnt = candyType.length / 2;
+        for (int i = 0; i < candyType.length; i++) {
+            if (!set.contains(candyType[i])) {
+                set.add(candyType[i]);
+                cnt --;
+            }
+            if (cnt == 0) {
+                break;
+            }
+        }
+        return candyType.length / 2 - cnt;
+    }
+}
