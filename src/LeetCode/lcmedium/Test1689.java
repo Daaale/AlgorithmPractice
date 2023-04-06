e787c2ea84275e31f1aab141ec8d55be161f844d
Dale <172586304@qq.com>
2023-04-06 22:59:18 +0800

    add lcmedium

---
+++ src/LeetCode/lcmedium/Test1689.java
@@ -0,0 +1,24 @@
+package LeetCode.lcmedium;
+
+/**
+ * @author Dale
+ * @create 2023-04-06 22:56
+ * @description
+ */
+public class Test1689 {
+    public static void main(String[] args) {
+        String n = "32";
+        int res = minPartitions(n);
+        System.out.println(res);
+    }
+    public static int minPartitions(String n) {
+        int res = 0;
+        for (int i = 0; i < n.length(); i++) {
+            char c = n.charAt(i);
+            if (res < c - '0') {
+                res = c - '0';
+            }
+        }
+        return res;
+    }
+}
