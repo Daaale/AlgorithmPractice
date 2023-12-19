52e0a0bd47c1e1745fe81e9ab600b59d0f7d6104
Daaale <172586304@qq.com>
2023-12-19 14:05:57 +0800

    add lceasy..

---
+++ src/main/java/LeetCode/lceasy/Test2744.java
@@ -0,0 +1,25 @@
+package LeetCode.lceasy;
+
+public class Test2744 {
+    public static void main(String[] args) {
+        String[] words = {"cd","ac","dc","ca","zz"};
+        int res = maximumNumberOfStringPairs(words);
+        System.out.println(res);
+    }
+    public static int maximumNumberOfStringPairs(String[] words) {
+        int res = 0;
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[i].length() != words[j].length()) continue;
+                boolean flag = true;
+                for (int k = 0; k < words[i].length(); k++) {
+                    if (words[i].charAt(k) != words[j].charAt(words[i].length() - k - 1)) {
+                        flag = false;
+                    }
+                }
+                if (flag) res ++;
+            }
+        }
+        return res;
+    }
+}
