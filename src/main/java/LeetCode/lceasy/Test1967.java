08d7aabc621389f412e4bb45941fc2cd56eec391
Daaale <172586304@qq.com>
2023-12-21 10:27:25 +0800

    add ..

---
+++ src/main/java/LeetCode/lceasy/Test1967.java
@@ -0,0 +1,17 @@
+package LeetCode.lceasy;
+
+public class Test1967 {
+    public static void main(String[] args) {
+        String[] patterns = {"a","abc","bc","d"};
+        String word = "abc";
+        int res = numOfStrings(patterns, word);
+        System.out.println(res);
+    }
+    public static int numOfStrings(String[] patterns, String word) {
+        int res = 0;
+        for (int i = 0; i < patterns.length; i++) {
+            if (word.contains(patterns[i])) res ++;
+        }
+        return res;
+    }
+}
