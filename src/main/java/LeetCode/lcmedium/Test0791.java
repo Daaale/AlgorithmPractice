f0c1ae2afc847949f1ec694dc63b8fa82a947e19
Daaale <172586304@qq.com>
2023-12-25 09:26:30 +0800

    add lcmedium..

---
+++ src/main/java/LeetCode/lcmedium/Test0791.java
@@ -0,0 +1,39 @@
+package LeetCode.lcmedium;
+
+import java.util.HashSet;
+
+public class Test0791 {
+    public static void main(String[] args) {
+        String order = "kqep";
+        String s = "pekeq";
+        String res = customSortString(order, s);
+        System.out.println(res);
+    }
+    public static String customSortString(String order, String s) {
+        StringBuilder sb = new StringBuilder();
+        // 记录order中字符
+        int[] cnts_s = new int[27];
+        HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            cnts_s[s.charAt(i) - 'a'] ++;
+            set.add(s.charAt(i));
+        }
+        int[] cnts_order = new int[27];
+        for (int i = 0; i < order.length(); i++) {
+            if (set.contains(order.charAt(i))) {
+                for (int j = 0; j < cnts_s[order.charAt(i) - 'a']; j++) {
+                    cnts_order[order.charAt(i) - 'a'] ++;
+                    sb.append(order.charAt(i));
+                }
+            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (cnts_order[s.charAt(i) - 'a'] > 0) {
+                cnts_order[s.charAt(i) - 'a'] --;
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
