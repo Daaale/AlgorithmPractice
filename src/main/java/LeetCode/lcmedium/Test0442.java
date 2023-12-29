41731040aa5d2c4d07bec6efac432b83d026ea1c
Daaale <172586304@qq.com>
2023-12-29 11:16:56 +0800

    add lcmedium..

---
+++ src/main/java/LeetCode/lcmedium/Test0442.java
@@ -0,0 +1,26 @@
+package LeetCode.lcmedium;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Test0442 {
+    public static void main(String[] args) {
+        int[] nums = {4,3,2,7,8,2,3,1};
+        List<Integer> res = findDuplicates(nums);
+        System.out.println(res.size());
+    }
+    public static List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                res.add(nums[i]);
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return res;
+    }
+}
