1ddf886219631dbce7db20eceee19fea9a694eda
Daaale <172586304@qq.com>
2024-01-02 15:38:58 +0800

    update...

--- src/main/java/test/ExcelTest.java
+++ src/main/java/test/ExcelTest.java
@@ -1,8 +1,10 @@
 package test;
 
+import com.sun.jdi.PathSearchingVirtualMachine;
+
 public class ExcelTest {
-
-
-
-
+    public static void main(String[] args) {
+        String file1 = ExcelTest.class.getResource("").getFile();
+        System.out.println(file1);
+    }
 }
