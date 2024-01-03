2e993271e93e2cef48039654ce8bace060ec8886
Daaale <172586304@qq.com>
2024-01-03 08:59:39 +0800

    add lceasy..

---
+++ src/main/java/LeetCode/lceasy/Test0832.java
@@ -0,0 +1,31 @@
+package LeetCode.lceasy;
+
+import java.util.Arrays;
+
+public class Test0832 {
+    public static void main(String[] args) {
+        int[][] image = {{1,1,0},{1,0,1},{0,0,0}};
+        int[][] res = flipAndInvertImage(image);
+        System.out.println(Arrays.toString(res[0]));
+    }
+    public static int[][] flipAndInvertImage(int[][] image) {
+
+        for (int i = 0; i < image.length; i++) {
+            // 翻转每一行
+            for (int j = 0; j < image[i].length / 2; j++) {
+                int temp = image[i][j];
+                image[i][j] = image[i][image.length - j - 1];
+                image[i][image.length - j - 1] = temp;
+            }
+            // 反转图片
+            for (int j = 0; j < image.length; j++) {
+                if (image[i][j] == 0) {
+                    image[i][j] = 1;
+                } else {
+                    image[i][j] = 0;
+                }
+            }
+        }
+        return image;
+    }
+}
