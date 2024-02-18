7d81fe496005bc1fee9becd8b410441529e5fadb
Daaale <172586304@qq.com>
2024-02-18 09:48:59 +0800

    add lcmedium and lceasy..

--- src/main/java/LeetCode/lceasy/test2000/Test1636.java
+++ src/main/java/LeetCode/lceasy/test2000/Test1636.java
@@ -1,14 +1,42 @@
 package LeetCode.lceasy.test2000;
 
-import java.util.Arrays;
+import java.util.*;
+
 
 public class Test1636 {
     public static void main(String[] args) {
-        int[] nums = {1,1,2,2,2,3};
+        int[] nums = {-1,1,-6,4,5,-6,1,4,1};
         int[] res = frequencySort(nums);
         System.out.println(Arrays.toString(res));
     }
     public static int[] frequencySort(int[] nums) {
-        return null;
+        Map<Integer, Integer> map = new TreeMap<Integer, Integer>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return o1.getValue() - o2.getValue();
+            }
+        });
+        ArrayList<Integer> temp = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : list) {
+            for (int i = 0; i < entry.getValue(); i++) {
+                temp.add(entry.getKey());
+            }
+        }
+        int[] res = new int[temp.size()];
+        for (int i = 0; i < temp.size(); i++) {
+            res[i] = temp.get(i);
+        }
+        return res;
     }
 }
--- src/main/java/LeetCode/lcmedium/test1000/Test0969.java
+++ src/main/java/LeetCode/lcmedium/test1000/Test0969.java
@@ -1,5 +1,6 @@
 package LeetCode.lcmedium.test1000;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Test0969 {
@@ -11,19 +12,38 @@ public class Test0969 {
         }
     }
     public static List<Integer> pancakeSort(int[] arr) {
-        return null;
-    }
-
-    public static int[] reverse(int[] arr, int l) {
-        int[] res = new int[arr.length];
-        for (int i = 0; i < l; i++) {
-            res[l - i - 1] = arr[i];
-        }
-        if (l < arr.length) {
-            for (int i = l; i < arr.length; i++) {
-                res[i] = arr[i];
+        List<Integer> res = new ArrayList<>();
+        int len = arr.length;
+        // 反转子数组 arr[0...k-1] ，所以要把最大值先搞到最后
+        while (len > 1) {
+            int maxValIndex = findMaxValIndex(arr, len - 1);
+            // 最大值不在最后
+            if (maxValIndex != len - 1) {
+                reverseArr(arr, 0, maxValIndex);
+                res.add(maxValIndex + 1);
+                reverseArr(arr, 0, len - 1);
+                res.add(len);
             }
+            len--;
         }
         return res;
+    }
+
+    public static int findMaxValIndex(int[] arr, int end) {
+        int res = 0;
+        for (int i = 0; i <= end; i++) {
+            if (arr[i] > arr[res]) res = i;
+        }
+        return res;
+    }
+
+    public static void reverseArr(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
